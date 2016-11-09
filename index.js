9c35ba6ca084d0d49817f8a2eee75043cc9fe273
Izel Nakri <contact@izelnakri.com>
2016-11-09 17:28:27 +0100

    Initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+// needs to know the students(names)
+var students = [
+  'Jessica', 'Marie', 'William', 'Zahra', 'Marco', 'Mike', 'Kevin', 'Isis', 'Ela', 'Lorenzo'
+];
+
+// iteraterate through the array and ask a question:
+students.forEach((student) => {
+  console.log('Is ' + student + ' attending?');
+});
+
+
+
+
+// Is studentName attending?
+// it will receive an answer if the answer is yes it will push that person to the attendingList
+
+
+// when the iteration finished print all the attendingList students
